91e0e6ccd16b195d45b4edbb9327aa0aa8db0ccf
silent-hyena <ayushpanwar00007@gmail.com>
2025-11-09 18:07:14 +0530

    new admission intro page

--- frontend/src/routes/NewAdmission/NewAdmission.jsx
+++ frontend/src/routes/NewAdmission/NewAdmission.jsx
@@ -1,73 +1,66 @@
-// import { Link } from "react-router-dom"
 import { useState } from "react"
 import Navbar from "../../components/Navbar/Navbar.jsx"
 import FormLogin from "../AdmissionForm/AdmissionForm.jsx"
 import ApplicationFormCheck from "../ApplicationStatus/AdmissionStatus.jsx"
 import RazorpayPayment from "../RazorpayPayment/Razorpay.jsx"
 import Footer from "../../components/Footer/AppFooter";
-
 import Sidebar from "../../components/QuickAccess/Sidebar/Sidebar.jsx"
-// import StudentPortal from "./StudentPortal.jsx"
-import AdminPage from "../AdminPage/AdminPage.jsx"
+import AdmissionIntro from "./AdmissionIntro.jsx"
 
 function NewAdmission() {
-    const [showAdmissionform, setShowAdmissionForm] = useState(true)
+    const [showIntro, setShowIntro] = useState(true);
+    const [showAdmissionform, setShowAdmissionForm] = useState(false)
     const [showpayment, setShowPayment] = useState(false);
     const [showApplcationStatus, setApplicationStatus] = useState(false)
-    // const [showPaymentForm, setPaymentForm] = useState(false)
-    // const [alert, setAlert] = useState(false)
-    // const [alertMessage, setAlertMessage] = useState(null)
-
-    // const { register, handleSubmit, formState: { errors } } = useForm();
 
     function handleFormBtn() {
+        setShowIntro(false);
         setShowAdmissionForm(true);
         setShowPayment(false);
         setApplicationStatus(false);
     }
 
     function handlePaymentBtn() {
+        setShowIntro(false);
         setShowAdmissionForm(false);
         setShowPayment(true);
         setApplicationStatus(false);
-        // window.location.href = "..Frontend/razorpay.html";
-
     }
+
     function handleFormCheck() {
+        setShowIntro(false);
         setApplicationStatus(true);
         setShowAdmissionForm(false);
         setShowPayment(false);
     }
 
-
-
-
     return (
         <>
-
             <Navbar />
 
-            {/* {alert && <AutoDismissAlert message={alertMessage} onClose={() => setAlert(false)} />} */}
-           
+            <div className="d-flex" style={{ backgroundColor: "rgb(240, 241, 242)" }}>
+                <div className="sidebar-wrapper">
+                    <Sidebar
+                        handleFormBtn={handleFormBtn}
+                        handlePaymentBtn={handlePaymentBtn}
+                        handleFormCheck={handleFormCheck}
+                    />
+                </div>
 
-            <div className="d-flex" style={{"backgroundColor": "rgb(240, 241, 242)"}}>
-                    <div className="sidebar-wrapper">
-                        <Sidebar
-                            handleFormBtn={handleFormBtn}
-                            handlePaymentBtn={handlePaymentBtn}
-                            handleFormCheck={handleFormCheck}
-                        />
-                    </div>
+                <div className="flex-grow-1 container-fluid">
+                    
+                    {showIntro && <AdmissionIntro onStart={handleFormBtn} />}
 
-                    <div className="flex-grow-1 container-fluid">
-                        {showAdmissionform && <FormLogin />}
-                        {showpayment && <RazorpayPayment />}
-                        {showApplcationStatus && <ApplicationFormCheck />}
-                    </div>
-             </div>
+                    {showAdmissionform && <FormLogin />}
+                    {showpayment && <RazorpayPayment />}
+                    {showApplcationStatus && <ApplicationFormCheck />}
+
+                </div>
+            </div>
+
             <Footer />
-
         </>
     )
 }
+
 export default NewAdmission
